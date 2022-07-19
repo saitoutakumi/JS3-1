607a46f65eb3ee3d0bf4fe2348029b21a60021ac
takumisaito <jobtech.saitou@gmail.com>
2022-07-19 16:40:48 +0900

    途中経過でご相談です③

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const tasks = document.getElementById('tasks');
+const output = document.getElementById('output');
 const input = document.getElementById('submit');
 
+const tasks = [];
+//配列を生成
 submit.addEventListener('click', function () {
-  // console.log(tasks.value);
-
-  const tasks = [
-    {
-      ID: '${index}',
-      comment: document.getElementById('tasks').value,
-      situation: '実行/削除ボタン',
-      //?IDが含まれていることになるのか
-    },
-  ];
-
-  const p = document.createElement('p');
-  output.appendChild(p);
-  tasks.forEach(
-    (tasks, index) =>
-      (p.innerHTML = `${index}  ${tasks.comment}  ${tasks.situation}`)
-  );
-  tasks.push(tasks.value);
-  //IDが0,1,2・・とならない
-  // ループできていないから？
   const text = document.getElementById('tasks');
+  const input = document.createElement('button');
+  input.type = 'submit';
+  button.value = '作業中';
+  button2.value = '削除';
+  tasks.push({
+    comment: text.value,
+    situation: input.value,
+  });
+  output.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const p = document.createElement('p');
+    output.appendChild(p);
+    p.innerHTML = `${index} ${task.comment} ${task.situation}`;
+  });
   text.value = '';
 });
-
-// まずやりたいこと
-//
-//=======================================================================
-// 入力されたタスクをオブジェクトで管理する？
-// →→入力された値を（①ID・②コメント・③状態）からなるオブジェクトを使って取得する
-// innerHTMLでID(No.)・コメント(タスクの内容)・状態ボタン(作業中デフォ)を出力
-
-// ①ID→は第一引数を表示させればナンバリングできる？
-// ②コメント(タスクの内容)→普通に出力
-// ③状態ボタン(作業中デフォ)→createElementでinput type=button辺りを生成？
